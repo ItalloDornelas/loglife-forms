eb0e65a2a20abe87db0384972d48487c30b93a7a
Itallo <dornelaszxd@gmail.com>
2021-07-20 16:00:20 -0300

    feat: autorizacao para entrar na pagina criado

---
+++ src/routes/route.tsx
@@ -0,0 +1,38 @@
+import { useEffect } from "react";
+import { ReactElement, useState } from "react";
+import { Redirect, Route as ReactDOMRoute } from "react-router-dom";
+import { useAuth } from "../provider/auth/auth";
+
+interface Props {
+  isPrivate?: boolean;
+  component: () => ReactElement;
+  path: string;
+  exact?: boolean;
+}
+
+const Route = ({ isPrivate = false, component: Component, ...rest }: Props) => {
+  const { auth } = useAuth();
+  const [token, setToken] = useState(localStorage.getItem("@token") || "");
+  useEffect(() => {
+    setToken(localStorage.getItem("@token") || "");
+  }, [auth]);
+  console.log(token);
+  return (
+    <ReactDOMRoute
+      {...rest}
+      render={() => {
+        return isPrivate === !!token ? (
+          <Component />
+        ) : (
+          <Redirect
+            to={{
+              pathname: isPrivate ? "/login" : "/dashboard",
+            }}
+          />
+        );
+      }}
+    />
+  );
+};
+
+export default Route;
